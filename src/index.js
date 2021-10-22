d09103626584114f73ee8c4d6645b97ffef1550f
Renan <renan.sergipe68@gmail.com>
2021-10-22 17:26:18 -0300

    Listando extrato

--- src/index.js
+++ src/index.js
@@ -13,15 +13,27 @@ app.post('/account', (request, response)=>{
   }
   const id = uuidv4()
 
-  const custumersAlreadyExists = customers.find(custumer => custumer.cpf === cpf)
+  const customersAlreadyExists = customers.some(customer => customer.cpf === cpf)
+  //some retorna true ou false, diferente do find que retorna o objeto
 
-  if(custumersAlreadyExists){
-    console.log(custumersAlreadyExists)
+  if(customersAlreadyExists){
+    console.log(customersAlreadyExists)
     return response.status(400).json({message: "Custumer Already exists"})
   }
 
-  customers.push({cpf, name, id})
+  customers.push({cpf, name, id, statement: []})
   response.status(201).send()
+})
+
+app.get('/statement/:cpf', (request, response)=>{
+  const { cpf } = request.params
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if(!customer){
+    return response.status(400).json({message: "Cpf not found"})
+  }
+  return response.json(customer.statement)
 })
 
 
